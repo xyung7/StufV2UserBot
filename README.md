70801eb8058f531b3c60cd14c0fa6bc711fbb88c
xyung7 <105676986+xyung7@users.noreply.github.com>
2022-07-09 19:03:47 +0700

    push

---
+++ README.md
@@ -0,0 +1,32 @@
+# Stufv2-Userbot Telegram
+
+Stufv2-Userbot adalah userbot Telegram modular yang berjalan di Python3 dengan Library Pyrogram.
+
+Saya membuat repository ini untuk bersenang-senang sekaligus membantu anda mengelola grup secara efisien dan untuk membantu mengurangi kebosanan saat menggunakan telegram.
+
+## ⚠️ Disclaimer
+
+```
+Saya tidak bertanggung jawab atas penyalahgunaan bot ini.
+Gunakan bot ini dengan risiko Anda sendiri.
+Gunakan userbot ini dengan bijak.
+Ketika anda sudah memasang userbot ini, berarti anda sudah siap dengan resikonya.
+```
+
+## Deploy heroku
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/xyung7/StufV2UserBot)
+
+## 👨🏻‍💻 Credits
+-  [Dan](https://github.com/delivrance) for [Pyrogram](https://github.com/pyrogram/pyrogram)
+-  [Risman](https://github.com/mrismanaziz) for [PyroMan-Userbot](https://github.com/mrismanaziz/PyroMan-Userbot)
+
+#### Special Thanks To [Everyone](https://github.com/mrismanaziz/PyroMan-Userbot/graphs/contributors) Who Has Helped Make This Userbot Awesome!
+-  [TeamDerUntergang](https://github.com/TeamDerUntergang/Telegram-SedenUserBot) : SedenUserBot
+-  [TheHamkerCat](https://github.com/TheHamkerCat/WilliamButcherBot) : WilliamButcherBot
+-  [TeamYukki](https://github.com/TeamYukki/YukkiMusicBot) : YukkiMusicBot
+-  [ITZ-ZAID](https://github.com/ITZ-ZAID) : Zaid-UserBot
+-  [Tofikdn](https://github.com/tofikdn) : Tede
+-  [Toni](https://github.com/Toni880) : Prime-UserBot
+
+## 📑 License
+Licensed under [GNU General Public License v3.0](https://github.com/mrismanaziz/PyroMan-Userbot/blob/Man-Userbot/LICENSE) All designs were created by [@mrismanaziz](https://github.com/mrismanaziz)
---
+++ config.py
@@ -0,0 +1,54 @@
+# Credits: @mrismanaziz
+# Copyright (C) 2022 Pyro-ManUserbot
+#
+# This file is a part of < https://github.com/mrismanaziz/PyroMan-Userbot/ >
+# PLease read the GNU Affero General Public License in
+# <https://www.github.com/mrismanaziz/PyroMan-Userbot/blob/main/LICENSE/>.
+#
+# t.me/SharingUserbot & t.me/Lunatic0de
+
+from base64 import b64decode
+from distutils.util import strtobool
+from os import getenv
+
+from dotenv import load_dotenv
+
+if os.path.exists("local.env"):
+    load_dotenv("local.env")
+que = {}
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1027174031").split()))
+API_ID = int(getenv("API_ID", ""))
+API_HASH = getenv("API_HASH", ""))
+LOG_CHAT = int(getenv("LOG_CHAT") or 0)
+SPOTIFY_CLIENT_SECRET = getenv("SPOTIFY_CLIENT_SECRET", "")
+SPOTIFY_CLIENT_ID = getenv("SPOTIFY_CLIENT_ID", "")
+SPOTIFY_USERNAME = getenv("SPOTIFY_USERNAME", "")
+MONGO_DB = getenv("MONGO_DB", "")
+ALIVE_IMG = getenv("ALIVE_IMG", "")
+DB_URL = getenv("DATABASE_URL", "")
+STRING_SESSION1 = getenv("STRING_SESSION1", "")
+STRING_SESSION2 = getenv("STRING_SESSION2", "")
+STRING_SESSION3 = getenv("STRING_SESSION3", "")
+STRING_SESSION4 = getenv("STRING_SESSION4", "")
+STRING_SESSION5 = getenv("STRING_SESSION5", "")
+GIT_TOKEN = getenv(
+    "GIT_TOKEN",
+    b64decode("Z2hwX1Z2Z2c2ZWdPaFVlcW8zNzRsSTh6aktIMTlrUEVNcTNBdFNuRw==").decode(
+        "utf-8"
+    ),
+)
+GROUP = getenv("GROUP", "stufgrup")
+HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
+HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
+PMPERMIT_PIC = getenv("PMPERMIT_PIC", None)
+PM_AUTO_BAN = strtobool(getenv("PM_AUTO_BAN", "True"))
+REPO_URL = getenv(
+    "REPO_URL",
+    b64decode("aHR0cHM6Ly9naXRodWIuY29tL1N0dWZVQi9TdHVmdjJVc2VyYm90").decode("utf-8"),
+)
+STRING_SESSION1 = getenv("STRING_SESSION1", "")
+STRING_SESSION2 = getenv("STRING_SESSION2", "")
+STRING_SESSION3 = getenv("STRING_SESSION3", "")
+STRING_SESSION4 = getenv("STRING_SESSION4", "")
+STRING_SESSION5 = getenv("STRING_SESSION5", "")
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
